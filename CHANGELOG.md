1487704cf2a79c0f2180df300c0fbaaa0422579d
Vahe1994 <vahe527887@yandex.ru>
2025-11-08 14:07:30 +0100

    platinum benchmark initial commit without changes

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+## Changelog
+
+### 2025-04-14
+
+We added GPT-4.1, GPT-4.5, Grok 3 beta, Grok 3 mini beta, Gemini 2.5 Pro, and Llama 4 Maverick. Following our data cleaning process, we reviewed the labels of examples on which either of these models failed.
+
+### 2025-02-26
+
+We added Claude 3.7 Sonnet and Claude 3.7 Sonnet (Thinking Mode) to our leaderboard. Following our data cleaning process, we reviewed the labels of examples on which either of these models failed.
+
+### 2025-02-13
+
+We add Gemini 2.0 Pro and Qwen2.5-Max to our leaderboard, and review the labels of any examples on which either of these models fail that have yet not been reviewed.
+
+### 2025-02-06
+
+This is the original release of the benchmark.
---
+++ scripts/get_results.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+python src/run_benchmark.py \
+    --model-list \
+        meta-llama/Meta-Llama-3.1-405B-Instruct \
+        claude-3-5-sonnet \
+        o1-mini \
+        gpt-4o-2024-11-20 \
+        gpt-4o-2024-08-06 \
+        gemini-1.5-pro \
+        mistral-large \
+        meta-llama/Meta-Llama-3.1-70B-Instruct \
+        gpt-4o-mini \
+        gemini-1.5-flash \
+        mistral-small \
+        o1-preview-2024-09-12 \
+        meta-llama/Llama-3.3-70B-Instruct \
+        grok-2-1212 \
+        claude-3-5-sonnet-20241022 \
+        claude-3-5-haiku \
+        gemini-2.0-flash \
+        gemini-2.0-flash-thinking \
+        deepseek/deepseek-chat \
+        Qwen/Qwen2.5-72B-Instruct \
+        o1-2024-12-17-med \
+        o1-2024-12-17-high \
+        deepseek-r1 \
+        o3-mini-2025-01-31-high \
+        gemini-2.0-pro-02-05 \
+        Qwen2.5-Max \
+        claude-3-7-sonnet-20250219 \
+        claude-3-7-sonnet-20250219-thinking \
+        gpt-4.5-preview-2025-02-27 \
+    --output-file ./outputs/live_results.csv \
+    --save-errors
